5faaff6e80495a38a2d6eb3f2c6ba054398d9cc9
Hiroyuki Yamaoka <yamaoka@10mado.jp>
2015-02-19 21:15:30 +0900

    initial commit

---
+++ src/Clover/Worker.php
@@ -0,0 +1,35 @@
+<?php
+namespace Clover;
+
+use Parallel\Prefork;
+
+abstract class Worker
+{
+    protected $defaultOptions = [
+        'max_workers' => 2,
+        'trap_signals' => [
+            SIGHUP => SIGTERM,
+            SIGTERM => SIGTERM,
+        ],
+    ];
+
+    public abstract function main();
+
+    public function start(array $ppOptions = [])
+    {
+        $options = array_merge($this->defaultOptions, $ppOptions);
+        $pp = new Prefork($options);
+
+        while ($pp->signalReceived() !== SIGTERM) {
+            if ($pp->start()) {
+                continue;
+            }
+
+            $this->main();
+
+            $pp->finish();
+        }
+
+        $pp->waitAllChildren();
+    }
+}
